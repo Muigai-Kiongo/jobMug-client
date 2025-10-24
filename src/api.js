6d5119b4deee9e23e88a5939c8250dce84662835
muigai-kiongo <kabochakiongo@gmail.com>
2025-10-24 20:39:21 +0300

    base url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000'
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://job-mug-api.vercel.app'
   // When using the Vite proxy, baseURL can be empty and /api/... will be proxied.
 });
 
